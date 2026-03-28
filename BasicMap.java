4abddb76dee80681c8bc560581c3b83e464541a7
Calum MacLeod <cdm27@st-andrews.ac.uk>
2026-03-28 14:30:29 +0000

    Mapping routes

--- BasicMap.java
+++ BasicMap.java
@@ -9,6 +9,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class BasicMap {
+   
     public static void main(String[] args) {
         JFrame frame = new JFrame("Map");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -36,15 +37,38 @@ public class BasicMap {
         GeoPosition london = new GeoPosition(51.5074, -0.1278);
 
         mapViewer.setAddressLocation(london);
-        mapViewer.setZoom(5);
+        mapViewer.setZoom(4);
 
         // Pan and zoom
         mapViewer.addMouseListener(new PanMouseInputListener(mapViewer));
         mapViewer.addMouseMotionListener(new PanMouseInputListener(mapViewer));
         mapViewer.addMouseWheelListener(new ZoomMouseWheelListenerCursor(mapViewer));
 
+        // Add marker
+            
+        Set<Waypoint> waypoints = new HashSet<>();
+        Router router = new Router();
+        Route route = router.createRoute(-0.1278, 51.5074, -0.081829, 51.530811);
+        for(double[] array : route.getCoordinates()) {
+            double lat = array[1];
+            double longitude = array[0];
+            System.out.println(lat);
+            System.out.println(longitude);
+            waypointPainter(waypoints, lat, longitude);
+        }
+        WaypointPainter<Waypoint> painter = new WaypointPainter<>();
+        painter.setWaypoints(waypoints);
+        mapViewer.setOverlayPainter(painter);
+
         frame.add(mapViewer);
         frame.setVisible(true);
+    }
 
+    public static void waypointPainter(Set<Waypoint> waypoints, double lat, double longitude) {
+        
+
+        Waypoint currentPoint = new DefaultWaypoint(lat, -longitude);
+        waypoints.add(currentPoint);
+       
     }
 }
