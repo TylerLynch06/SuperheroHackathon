d9a764a622364b69ab1dd2155cc02a6710497f03
Tyler Lynch <tl217@st-andrews.ac.uk>
2026-03-28 11:37:03 +0000

    Basic routing

--- Routing.java
+++ Routing.java
@@ -1,3 +1,28 @@
-public class Routing {
+import java.net.*;
+import java.io.*;
+import java.util.stream.Collectors;
 
+public class Routing {
+    public static void main(String[] args) throws Exception {
+        
+        // London to Manchester (longitude,latitude format)
+        double startLon = -0.1276, startLat = 51.5074;
+        double endLon = -2.2426,   endLat = 53.4808;
+
+        String url = "http://router.project-osrm.org/route/v1/driving/"
+                   + startLon + "," + startLat + ";"
+                   + endLon   + "," + endLat
+                   + "?overview=false";
+
+        HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+        conn.setRequestMethod("GET");
+
+        BufferedReader in = new BufferedReader(
+            new InputStreamReader(conn.getInputStream())
+        );
+        String response = in.lines().collect(Collectors.joining());
+        in.close();
+
+        System.out.println(response);
+    }
 }
